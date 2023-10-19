7d51028d598eeeacd835afc987fc3552b4ba7876
IT21174308 <it21174308@my.sliit.lk>
2023-10-19 15:54:48 +0530

    conflictsolved

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,37 +1,71 @@
 import express, { Express, Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import logger from '../log/logger';
-import { accountRoute } from './routes/account.route'
 
+import bodyParser from 'body-parser';
+
+import mongoose from 'mongoose';
+
+import cors from 'cors';
+
+import logger from '../log/logger';
+
+import { accountRoute } from './routes/account.route';
+
+import { paymentRoute } from './routes/payment.route';
+
+import { userRoute } from './routes/user.route';
+
+ 
 
 require('dotenv').config();
 
+ 
+
 const app: Express = express();
+
 const port = process.env.PORT;
 
+ 
+
 app.use(cors());
+
 app.use(bodyParser.json());
+
 app.use(
+
   bodyParser.urlencoded({
+
     extended: true,
+
   }),
+
 );
+
+ 
 
 app.use('/api/account', accountRoute);
 
-const app = express();
-app.use(express.json());
+app.use('/api/payment', paymentRoute);
 
-// Import your worker routes
-import workerRoutes from './routes/admin.route';
+app.use('/api/user', userRoute);
 
-// Use your worker routes
-app.use('/workers', workerRoutes);
+ 
 
-const port = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGODB_URI).then(() => {
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  logger.info('MongoDB connected');
+
+  app.on('error', (err) => {
+
+    logger.error(err);
+
+  });
+
+  app.listen(port, () => {
+
+    logger.info(`TypeScript with Express
+
+         http://localhost:${port}/`);
+
+  });
+
 });
